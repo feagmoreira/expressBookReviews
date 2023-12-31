136e91812ed72d11031ab5c1fe412e84a9e5cee6
Felipe Gebara Moreira <feagmoreira@hotmail.com>
2023-12-30 23:47:25 -0500

    Tasks 3, 4, 5, 6 and 7

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,20 +32,42 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  let matchingAuthors = [];
+  if (author){
+    for (let key in books){
+        if(books[key]["author"] === author){
+            matchingAuthors.push(books[key]); 
+        }
+    }
+    if (matchingAuthors.length > 0){
+        res.send(matchingAuthors);
+    }   
+  }
+  return res.status(404).json({message: "No book found for this author."});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    let title = req.params.title;
+    let matchingTitles = [];
+    if (title){
+      for (let key in books){
+          if(books[key]["title"] === title){
+              matchingTitles.push(books[key]); 
+          }
+      }
+      if (matchingTitles.length > 0){
+          res.send(matchingTitles);
+      }   
+    }
+    return res.status(404).json({message: "No book found with this title."});
+  });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn
+  res.send(books[isbn]["reviews"]);
 });
 
 module.exports.general = public_users;
